07f9455da533620f4489ba974940969dca07963c
Evavic44 <evavic44@gmail.com>
2021-08-12 15:53:00 +0100

    Implement scroll animation

--- js/script.js
+++ js/script.js
@@ -16,3 +16,30 @@ function closeMenu() {
    hamburger.classList.remove("active");
    navMenu.classList.remove("active");
 }
+
+// Navbar scroll effect
+const navbar = document.getElementById('navbar');
+const link = document.getElementById('link');
+const link2 = document.getElementById('link2');
+const link3 = document.getElementById('link3');
+const link4 = document.getElementById('link4');
+const logo = document.getElementById('logo'); 
+let scrolled = false;
+
+window.onscroll = function() {
+   if(window.pageYOffset > 100) {
+      navbar.classList.remove('top');
+      link.classList.remove('colors');
+      link2.classList.remove('colora');
+      link3.classList.remove('colorb');
+      link4.classList.remove('colorc');
+      logo.classList.remove('colorz')
+   } else {
+      navbar.classList.add('top');
+      link.classList.add('colors');
+      link2.classList.add('colora');
+      link3.classList.add('colorb');
+      link4.classList.add('colorc');
+      logo.classList.add('colorz');
+   }
+}
