1e3cfc725acdfc888becd709af3b375bd6934a1f
Evavic44 <evavic44@gmail.com>
2021-08-24 14:06:01 +0100

    Add counter script

--- js/script.js
+++ js/script.js
@@ -43,3 +43,25 @@ window.onscroll = function() {
       logo.classList.add('colorz');
    }
 }
+
+// Numbers counter script
+const counters = document.querySelectorAll('.counter');
+const speed = 1000;
+
+counters.forEach(counter => {
+   const updateCount = () => {
+      const target = +counter.getAttribute('data-target');
+      const count = +counter.innerText;
+
+      const inc = target / speed;
+
+      if(count < target) {
+         counter.innerText = Math.ceil(count + inc);
+         setTimeout(updateCount, 10);
+      } else {
+         count.innerText = target;
+      }
+   }
+
+   updateCount()
+});
